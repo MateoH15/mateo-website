157fd953297558e12306b6fd0917f7b7f5d2f06e
Mateo <mateohe091@gmail.com>
2026-02-10 12:32:26 -0300

    button whatsapp added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import CTA from "./components/CTA";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import WhatsAppButton from "./components/WhatsAppButton";
 import "./styles/index.css";
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
+      <WhatsAppButton />
     </>
   );
 }
